f0147897c06a3d3139bdfce9b75fa20e6c36f77d
Garrett Breach <breachg1992@gmail.com>
2020-10-26 16:56:57 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,10 +3,10 @@ var array = [0,1]
 function forLoop(array) {
   for (let i = 0; i<25; i++){
     if (i == 1) {
-      array.push('I am 1 strange loop.')
+      array.unshift('I am 1 strange loop.')
     }
     else {
-      array.push(`I am ${i} strange loops`)
+      array.unshift(`I am ${i} strange loops`)
     }
   }
 }
